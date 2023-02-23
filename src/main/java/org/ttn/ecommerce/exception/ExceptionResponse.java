cef6c60e5ed07c9ff5bea89956460db201fba183
kamlesh singh <30825138+kamlesh78@users.noreply.github.com>
2023-02-23 14:57:55 +0530

    Update ExceptionResponse.java

--- src/main/java/org/ttn/ecommerce/exception/ExceptionResponse.java
+++ src/main/java/org/ttn/ecommerce/exception/ExceptionResponse.java
@@ -1,19 +1,42 @@
 package org.ttn.ecommerce.exception;
 
 import java.time.LocalDateTime;
-@Getter
-@Setter
-public class ExceptionResponse{
-  LocalDateTime timestamp;
-  String message;
-  String details;
-  
-  ExceptionResponse(LocalDateTime timestamp,String message,String details){
-  this.timestamp=timestamp;
-  this.message=message;
-  this.details=details;
-  }
-  
-  ExceptionResponse(){}
+ 
+public class ExceptionResponse {
 
+    LocalDateTime timestamp;
+    String message;
+    String details;
+
+    ExceptionResponse(LocalDateTime timestamp,String message,String details){
+        this.timestamp=timestamp;
+        this.message=message;
+        this.details=details;
+    }
+
+    ExceptionResponse(){}
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
 }
