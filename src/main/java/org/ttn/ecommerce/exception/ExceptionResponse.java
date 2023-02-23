30f8dd7052b7406cabfbc292218836256ec83c57
kamlesh singh <30825138+kamlesh78@users.noreply.github.com>
2023-02-23 14:48:48 +0530

    Update ExceptionResponse.java

--- src/main/java/org/ttn/ecommerce/exception/ExceptionResponse.java
+++ src/main/java/org/ttn/ecommerce/exception/ExceptionResponse.java
@@ -2,7 +2,7 @@ package org.ttn.ecommerce.exception;
 
 import java.time.LocalDateTime;
 
-public record ExceptionResponse(LocalDateTime timestamp,
+public class ExceptionResponse(LocalDateTime timestamp,
                                 String message,
                                 String details) {
 
