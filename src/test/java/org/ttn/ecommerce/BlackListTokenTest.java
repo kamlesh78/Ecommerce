f529658ab0f97101952d204e2c5650a884f2e0de
kamlesh singh <30825138+kamlesh78@users.noreply.github.com>
2023-02-23 15:03:02 +0530

    Update BlackListTokenTest.java

--- src/test/java/org/ttn/ecommerce/BlackListTokenTest.java
+++ src/test/java/org/ttn/ecommerce/BlackListTokenTest.java
@@ -1,26 +1,26 @@
-package org.ttn.ecommerce;
+// package org.ttn.ecommerce;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.ttn.ecommerce.repository.TokenRepository.BlackListTokenRepository;
-import org.ttn.ecommerce.repository.categoryRepository.CategoryRepository;
-import org.ttn.ecommerce.services.impl.LogoutServiceImpl;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.ttn.ecommerce.repository.TokenRepository.BlackListTokenRepository;
+// import org.ttn.ecommerce.repository.categoryRepository.CategoryRepository;
+// import org.ttn.ecommerce.services.impl.LogoutServiceImpl;
 
-@SpringBootTest
-public class BlackListTokenTest {
+// @SpringBootTest
+// public class BlackListTokenTest {
 
-    @Autowired
-    BlackListTokenRepository blackListTokenRepository;
+//     @Autowired
+//     BlackListTokenRepository blackListTokenRepository;
 
-    @Autowired
-    LogoutServiceImpl blackListTokenService;
+//     @Autowired
+//     LogoutServiceImpl blackListTokenService;
 
-    @Autowired
-    CategoryRepository categoryRepository;
+//     @Autowired
+//     CategoryRepository categoryRepository;
 
-    @Test
-    public void testBlackListService(){
+//     @Test
+//     public void testBlackListService(){
 
-    }
-}
+//     }
+// }
